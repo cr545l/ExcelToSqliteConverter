703dc4f62aabd07e888466c0c4dfc947254c1760
cr545l <cr545l@gmail.com>
2016-12-01 18:12:10 +0900

    0.0.2

---
+++ xlsToSqliteConverter/Converter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Lofle.XlsToSqliteConverter
+{
+	class Converter
+	{
+		static private Excel.Application _application = new Excel.Application();
+		static private readonly string[] _extras = new[] { ".xls", ".xlsx" };
+
+		static public void Directorys( string[] directorys )
+		{
+			for( int i = 0; i < directorys.Length; i++ )
+			{
+				Debug.Log( "D {0}/{1} {2}", i + 1, directorys.Length, directorys[i] );
+				string[] files = Directory.GetFiles( directorys[i], "*.*", SearchOption.TopDirectoryOnly ).Where( s => _extras.Contains( Path.GetExtension( s ), StringComparer.OrdinalIgnoreCase ) ).ToArray();
+				Files( files );
+			}
+		}
+
+		static public void Files( string[] files )
+		{
+			for( int i = 0; i < files.Length; i++ )
+			{
+				Debug.Log( "F {0}/{1} {2}", i + 1, files.Length, files[i] );				
+				ExcelAssist.Read( _application, files[i] );
+			}
+		}
+	}
+}
---
+++ xlsToSqliteConverter/Debug.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lofle.XlsToSqliteConverter
+{
+	class Debug
+	{
+		static public void Log( string format, params object[] arg )
+		{
+			System.Console.WriteLine( format, arg );
+		}
+
+		static public void Log( string arg )
+		{
+			System.Console.WriteLine( arg );
+		}
+
+		static public void LogError( string arg )
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			System.Console.WriteLine( arg );
+			Console.ReadKey();
+			Console.ForegroundColor = ConsoleColor.White;
+		}
+
+		static public void LogError( string format, params object[] arg )
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			System.Console.WriteLine( format, arg );
+			Console.ReadKey();
+			Console.ForegroundColor = ConsoleColor.White;
+		}
+	}
+}
